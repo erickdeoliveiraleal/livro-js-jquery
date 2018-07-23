4b2fd50632cf7c8d07353b735316947438aa5227
ERICKDEOLIVEIRALEAL <ERICKDEOLIVEIRALEAL@GMAIL.COM>
2018-07-23 13:24:54 -0300

    bugs intellij

--- capitulo_05/javascripts/rogus.js
+++ capitulo_05/javascripts/rogus.js
@@ -1,45 +1,55 @@
 function moneyTextToFloat(text) {
-	var cleanText = text.replace("R$ ", "").replace(",", ".");
-	return parseFloat(cleanText);
+    var cleanText = text.replace("R$ ", "").replace(",", ".");
+    return parseFloat(cleanText);
 }
 
 function floatToMoneyText(value) {
-	var text = (value < 1 ? "0" : "") + Math.floor(value * 100);
-	text = "R$ " + text;
-	return text.substr(0, text.length - 2) + "," + text.substr(-2);
+    var text = (value < 1 ? "0" : "") + Math.floor(value * 100);
+    text = "R$ " + text;
+    return text.substr(0, text.length - 2) + "," + text.substr(-2);
 }
 
 function readTotal() {
-	var total = document.getElementById("total");
-	return moneyTextToFloat(total.innerHTML);
+    var total = document.getElementById("total");
+    return moneyTextToFloat(total.innerHTML);
 }
 
 function writeTotal(value) {
-	var total = document.getElementById("total");
-	total.innerHTML = floatToMoneyText(value);
+    var total = document.getElementById("total");
+    total.innerHTML = floatToMoneyText(value);
 }
 
 function calculateTotalProducts() {
-	var items = document.getElementsByClassName("item");
+    var produtos = document.getElementsByClassName("produto");
 
-	var totalProducts = 0;
+    var totalProdutos = 0;
 
-	for(var pos = 0; pos < items.length; pos++) {
-		var priceElements = items[pos].getElementsByClassName("price");
-		var priceText = priceElements[0].innerHTML;
-		var price = moneyTextToFloat(priceText);
+    for(var pos = 0; pos < produtos.length; pos++) {
+        var priceElements = produtos[pos].getElementsByClassName("price");
+        var priceText = priceElements[0].innerHTML;
+        var price = moneyTextToFloat(priceText);
 
-		var qtyElements = items[pos].getElementsByClassName("quantity");
-		var qtyText = qtyElements[0].value;
-		var quantity = moneyTextToFloat(qtyText);
+        var qtyElements = produtos[pos].getElementsByClassName("quantity");
+        var qtyText = qtyElements[0].value;
+        var quantity = moneyTextToFloat(qtyText);
 
-        totalProducts += quantity * price;
+        totalProdutos += quantity * price;
 
-	}
+    }
 
-	return totalProducts;
+    return totalProdutos;
 }
 
-function getFreightPrice() {
-	
+function onQuantityChange() {
+    writeTotal(calculateTotalProducts());
 }
+
+function onDocumentLoad() {
+    var textEdits = document.getElementsByClassName("quantity");
+
+    for(var i = 0; i < textEdits.length; i++) {
+        textEdits[i].onchange = onQuantityChange;
+    }
+}
+
+window.onload = onDocumentLoad;
